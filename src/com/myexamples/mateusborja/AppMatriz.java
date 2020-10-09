0953a000bee9a8f4fa4ac2e00b2ad8d41403ecc9
mateus.borja <mateus.borja@gmail.com>
2020-10-09 19:46:17 -0400

    Aplicativo de calculo de matriz

---
+++ src/com/myexamples/mateusborja/AppMatriz.java
@@ -0,0 +1,72 @@
+package com.myexamples.mateusborja;
+
+import java.io.*;
+import javax.swing.JOptionPane;
+
+public class AppMatriz {
+
+    public int mat[][];
+    int aux = 0;
+    DataInputStream ler = new DataInputStream(System.in);
+
+    public AppMatriz(int i) {
+        mat = new int[i][i];
+        aux = i;
+    }
+
+    public int[][] leitura() {
+        for (int i = 0; i < (aux); i++) {
+            for (int j = 0; j < (aux); j++) {
+                mat[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor para " + (i + 1) + "ª linha e da " + (j + 1) + "ª coluna "));
+            }
+        }
+        System.out.println();
+        return this.mat;
+    }
+
+    public int[][] adicao(int mat1[][], int mat2[][], int tam) {
+        int a[][] = new int[tam][tam];
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                a[i][j] = mat1[i][j] + mat2[i][j];
+            }
+        }
+        return a;
+    }
+
+    public int[][] subtracao(int mat1[][], int mat2[][], int tam) {
+        int a[][] = new int[tam][tam];
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                a[i][j] = mat1[i][j] - mat2[i][j];
+            }
+        }
+        return a;
+    }
+
+    public void mostra(int aux[][], int x) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(aux[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] multiplica(int mat1[][], int mat2[][], int tam) {
+        int s, aux;
+        int a[][] = new int[tam][tam];
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                aux = 0;
+                for (int k = 0; k < tam; k++) {
+                    aux += (mat1[i][k] * mat2[k][j]);
+                }
+                a[i][j] = aux;
+            }
+        }
+
+        return a;
+    }
+
+}
---
+++ src/com/myexamples/mateusborja/MainAppMatriz.java
@@ -0,0 +1,48 @@
+package com.myexamples.mateusborja;
+
+import javax.swing.*;
+
+public class MainAppMatriz {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+
+        x = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho da matriz quadrada"));
+
+        AppMatriz primeira = new AppMatriz(x);
+        AppMatriz segunda = new AppMatriz(x);
+
+        int mat1[][] = new int[x][x];
+
+        int mat2[][] = new int[x][x];
+
+        int mat3[][] = new int[x][x];
+
+        mat1 = primeira.leitura();
+        System.out.println();
+        mat2 = segunda.leitura();
+        System.out.println();
+
+        System.out.println("MATRIZES: ");
+        primeira.mostra(mat1, x);
+        System.out.println();
+
+        primeira.mostra(mat2, x);
+        System.out.println();
+
+        mat3 = primeira.adicao(mat1, mat2, x);
+        System.out.println("SOMA: ");
+        primeira.mostra(mat3, x);
+        System.out.println();
+
+        mat3 = primeira.subtracao(mat1, mat2, x);
+        System.out.println("SUBTRACAO: ");
+        primeira.mostra(mat3, x);
+        System.out.println();
+
+        mat3 = primeira.multiplica(mat1, mat2, x);
+        System.out.println("MULTIPLICACAO: ");
+        primeira.mostra(mat3, x);
+    }
+}
