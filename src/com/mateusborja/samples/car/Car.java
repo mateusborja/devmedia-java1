f4e8e74f74776aacb66be5556f307ae136682c40
mateus.borja <mateus.borja@gmail.com>
2020-10-17 22:18:16 -0400

    java modifiers

--- src/com/mateusborja/samples/car/Car.java
+++ src/com/mateusborja/samples/car/Car.java
@@ -1,6 +1,24 @@
+//java modifiers
 package com.mateusborja.samples.car;
 
 public class Car {
+
+    private static String color;
+    private static String client;
+    private static int age;
+
+    static {
+        color = "blue";
+        client = "Joanathas";
+        age = 1996;
+    }
+
+
+    public Car(String color, String client) {
+        this.color = color;
+        this.client = client;
+
+    }
 
     public void myCar() {
         System.out.println("Meu carro é azul...");
--- src/com/mateusborja/samples/car/MainCar.java
+++ src/com/mateusborja/samples/car/MainCar.java
@@ -2,8 +2,10 @@ package com.mateusborja.samples.car;
 
 public class MainCar {
     public static void main(String[] args) {
-        Car getAno = new Car();
+        Car getAno = new Car("Blue", "Jonathas");
         getAno.myCar();
+
+
 
     }
 }
