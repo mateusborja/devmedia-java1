04cc3110cbd3d766b1d3117bcb213f184c057590
mateus.borja <mateus.borja@gmail.com>
2020-10-19 00:16:21 -0400

    java operators

---
+++ src/com/mateusborja/samples/expressoes/Exemplo02.java
@@ -0,0 +1,26 @@
+package com.mateusborja.samples.expressoes;
+
+public class Exemplo02 {
+
+    public static void main(String[] args) {
+
+        int x, y, operacao;
+
+        x = 15;
+        y = 4;
+        operacao = x % y; //15 dividido por 4 igual a 3 = 15 menos 12  = 3 (logo resto e 3)
+        System.out.println(operacao);
+
+        x = 25;
+        y = 6;
+        operacao = x % y;
+        System.out.println(operacao);
+
+        x = 18;
+        y = 4;
+        operacao = x % y;
+        System.out.println(operacao);
+
+
+    }
+}
