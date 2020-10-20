ba2dbfb76223dca329351893ef8d58f8b8e7ba2d
mateus.borja <mateus.borja@gmail.com>
2020-10-19 20:48:10 -0400

    java casting and types

---
+++ src/com/mateusborja/javabegin/estruturasequencial/processamento/Exemplo03.java
@@ -0,0 +1,22 @@
+//java types
+package com.mateusborja.javabegin.estruturasequencial.processamento;
+
+public class Exemplo03 {
+
+    public static void main(String[] args) {
+
+        //area trapezio
+        double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = ((b + B) / 2) * h;
+
+        System.out.println(area);
+
+    }
+
+
+}
