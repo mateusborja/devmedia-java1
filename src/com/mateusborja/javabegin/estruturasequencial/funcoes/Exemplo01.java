c66d3e28091a1c9c06e3b1fde7171200b7f7d00e
mateus.borja <mateus.borja@gmail.com>
2020-10-19 21:26:32 -0400

    java function Math

---
+++ src/com/mateusborja/javabegin/estruturasequencial/funcoes/Exemplo01.java
@@ -0,0 +1,27 @@
+//java function Math.sqrt (x), Math.pow(x,Y) and Math.abs (x)
+package com.mateusborja.javabegin.estruturasequencial.funcoes;
+
+public class Exemplo01 {
+
+    public static void main(String[] args) {
+
+        double x, y,z ;
+        double A, B, C;
+
+        x = 3.0;
+        y = 16.0;
+        z = -5.0;
+
+        //raiz quadrada
+        A = Math.sqrt(y);
+        System.out.println("A raiz quadrada de " + y + " é igual a: " + A);
+
+        //potencia
+        B = Math.pow(x, x);
+        System.out.println(x + " elevado a " + x + " é igual a: " + B);
+
+        C = Math.abs(z);
+        System.out.println("O Valor absoluto de " + z +  " é igual a: " + C);
+
+    }
+}
