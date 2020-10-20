574927b51ca265618c5d70bb2cc5c4905b6ee89e
mateus.borja <mateus.borja@gmail.com>
2020-10-19 20:35:43 -0400

    java printf method

---
+++ src/com/mateusborja/samples/saidadedados/Exercicio01.java
@@ -0,0 +1,31 @@
+package com.mateusborja.samples.saidadedados;
+
+import java.util.Locale;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+
+
+        String product1 = "Computer";
+        String product2 = "Office Desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
+        System.out.printf("%s, wich price is $ %.2f%n%n", product2, price2);
+        System.out.printf("Records: %d years old, code %d and gender: %s%n%n", age, code, gender);
+        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rouded (three decimal places): %.2f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.2f%n", measure);
+
+    }
+}
