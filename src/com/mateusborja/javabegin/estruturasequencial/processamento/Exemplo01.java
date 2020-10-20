3998621f4d0edd85a557202700a8dc3385c13c2c
mateus.borja <mateus.borja@gmail.com>
2020-10-19 20:42:45 -0400

    java data types

---
+++ src/com/mateusborja/javabegin/estruturasequencial/processamento/Exemplo01.java
@@ -0,0 +1,18 @@
+package com.mateusborja.javabegin.estruturasequencial.processamento;
+
+public class Exemplo01 {
+
+    public static void main(String[] args) {
+
+        int x, y;
+
+        x = 10;
+        y = 5;
+
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+
+}
