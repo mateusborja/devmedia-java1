6e144bd04b8448c7d72b39317a382e612e9e6cf2
mateus.borja <mateus.borja@gmail.com>
2020-10-19 20:56:06 -0400

    java method printf

---
+++ src/com/mateusborja/javabegin/estruturasequencial/processamento/Exemplo05.java
@@ -0,0 +1,17 @@
+package com.mateusborja.javabegin.estruturasequencial.processamento;
+
+public class Exemplo05 {
+
+    public static void main(String[] args) {
+
+        double x;
+        int y;
+
+        x = 15.0;
+        y = (int) x;
+
+        System.out.printf("%.1f%n%d", x, y); // printf
+
+    }
+
+}
